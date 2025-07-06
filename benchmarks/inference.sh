aba8a9ffabd4b4314daa0a7566ab7d9b5e815621
Pei Mu <ds1231h@gmail.com>
2025-07-06 14:42:08 +0100

    Update inference.sh

--- benchmarks/inference.sh
+++ benchmarks/inference.sh
@@ -1,10 +1,10 @@
 
 #! /bin/bash
 
-for i in {1..5}; do python3 benchmarks/cutlass/cutlass_mm_leakyrelu.py --load auto --bench 1; done
-for i in {1..5}; do python3 benchmarks/batch_matmul.py --load auto --bench 1; done
-for i in {1..5}; do python3 benchmarks/fused_feedforward.py --load auto --bench 1; done
-for i in {1..5}; do python3 benchmarks/02-fused-softmax.py --load auto --bench 1; done
-for i in {1..5}; do python3 benchmarks/cutlass/torch_rmsnorm.py --load auto --bench 1; done
-for i in {1..5}; do python3 benchmarks/06-fused-attention.py --load auto --bench 1; done
+for i in {1..5}; do python3 benchmarks/cutlass/cutlass_mm_leakyrelu.py --load auto --bench; done
+for i in {1..5}; do python3 benchmarks/batch_matmul.py --load auto --bench; done
+for i in {1..5}; do python3 benchmarks/fused_feedforward.py --load auto --bench; done
+for i in {1..5}; do python3 benchmarks/02-fused-softmax.py --load auto --bench; done
+for i in {1..5}; do python3 benchmarks/cutlass/torch_rmsnorm.py --load auto --bench; done
+for i in {1..5}; do python3 benchmarks/06-fused-attention.py --load auto --bench; done
 
